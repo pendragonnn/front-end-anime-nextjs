1b78b22db351f10d478788fb9562bb0ecc8819e0
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-11-27 23:33:48 +0700

    feat(auth): implement logout feature and move header + footer component to layout.tsx

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,23 +1,17 @@
 export default function AnimePage() {
-
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100">
+    <section className="relative w-full h-[350px] sm:h-[420px] md:h-[480px] overflow-hidden shadow-lg">
+      {/* Title */}
+      <h2 className="text-3xl font-semibold text-blue-700 mb-4">
+        Daftar Anime
+      </h2>
 
-      {/* CONTENT */}
-      <main className="px-6 py-10 max-w-4xl mx-auto">
-        
-        {/* Title */}
-        <h2 className="text-3xl font-semibold text-blue-700 mb-4">Daftar Anime</h2>
+      <p className="text-gray-600 mb-6">Kamu sudah berhasil login!</p>
 
-        <p className="text-gray-600 mb-6">
-          Kamu sudah berhasil login! 
-        </p>
-
-        {/* Placeholder box */}
-        <div className="bg-white border border-blue-200 p-6 rounded-xl shadow-sm text-center text-gray-500">
-          <p>Belum ada data anime untuk ditampilkan.</p>
-        </div>
-      </main>
-    </div>
+      {/* Placeholder box */}
+      <div className="bg-white border border-blue-200 p-6 rounded-xl shadow-sm text-center text-gray-500">
+        <p>Belum ada data anime untuk ditampilkan.</p>
+      </div>
+    </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from "react";
 import LoginModal from "./components/auth/LoginModal";
 import Image from "next/image";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import RegisterModal from "./components/auth/RegisterModal";
 
 export default function HomePage() {
@@ -32,44 +30,36 @@ export default function HomePage() {
 
   return (
     <>
-      <Header />
+      <section className="relative w-full h-[350px] sm:h-[420px] md:h-[480px] overflow-hidden shadow-lg">
+        <Image
+          src="https://i.pinimg.com/736x/f1/b5/e3/f1b5e3ebd3fc9e685115d0b75410c8d0.jpg"
+          fill
+          priority
+          alt="Hero"
+          className="object-cover"
+        />
 
-      <main className="w-full mx-auto max-w-4xl px-4 sm:px-6 md:px-8 py-10">
-        <section className="relative w-full h-[350px] sm:h-[420px] md:h-[480px] overflow-hidden shadow-lg">
-          <Image
-            src="https://i.pinimg.com/736x/f1/b5/e3/f1b5e3ebd3fc9e685115d0b75410c8d0.jpg"
-            fill
-            priority
-            alt="Hero"
-            className="object-cover"
-          />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-blue-100/80"></div>
 
-          <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-blue-100/80"></div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white drop-shadow-md px-4 text-center">
+          <p className="text-md sm:text-lg md:text-xl font-light">🎌</p>
 
-          <div className="absolute inset-0 flex flex-col justify-center items-center text-white drop-shadow-md px-4 text-center">
-            <p className="text-md sm:text-lg md:text-xl font-light">🎌</p>
+          <h2 className="text-xl font-extrabold mb-2">Cari Anime Apa?</h2>
 
-            <h2 className="text-xl font-extrabold mb-2">
-              Cari Anime Apa?
-            </h2>
+          <p className="text-sm font-light">
+            {loggedIn ? "Yok Mas langsung masuk!" : "Login dulu yaa"}
+          </p>
 
-            <p className="text-sm font-light">
-              {loggedIn ? "Yok Mas langsung masuk!" : "Login dulu yaa"}
-            </p>
-
-            <div className="mt-8 flex justify-center">
-              <button
-                onClick={handleStart}
-                className="bg-transparent border text-white px-4 py-2 cursor-pointer shadow-lg transition-all active:scale-95"
-              >
-                {loggedIn ? "Masuk Sini" : "Mulai Sekarang"}
-              </button>
-            </div>
+          <div className="mt-8 flex justify-center">
+            <button
+              onClick={handleStart}
+              className="bg-transparent border text-white px-4 py-2 cursor-pointer shadow-lg transition-all active:scale-95"
+            >
+              {loggedIn ? "Masuk Sini" : "Mulai Sekarang"}
+            </button>
           </div>
-        </section>
-      </main>
-
-      <Footer />
+        </div>
+      </section>
 
       <LoginModal
         open={openLogin}
